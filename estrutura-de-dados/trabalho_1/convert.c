3c5820bed664632b3e8d78a7db0ae73cbab7aa15
ViniciusGCaetano <viny6888@gmail.com>
2023-03-10 23:54:20 -0300

    testing some things

--- estrutura-de-dados/trabalho_1/convert.c
+++ estrutura-de-dados/trabalho_1/convert.c
@@ -13,28 +13,20 @@ int main()
         printf("file can't be opened \n");
     }
  
-    int MAX_LEN = 300;
+    int MAX_LEN = 10000;
     char buf[] = "";
 
 
-    while (fgets(buf, MAX_LEN - 1, ptr))
-    {
-        printf("%s", buf);
-    }   
-
  
-    // Printing what is written in file
-    // character by character using loop.
-    // do {
-    //     ch = fgetc(ptr);
-    //     if(ch == '\n'){
-    //         printf("olha a quebra de linha!");
-    //     }
-    //     printf("%c", ch);
- 
-    //     // Checking if character is not EOF.
-    //     // If it is EOF stop reading.
-    // } while (ch != EOF);
+   
+    do {
+        ch = fgetc(ptr);
+        if(ch == '\n'){
+            printf("olha a quebra de linha!");
+       }
+       printf("%c", ch);
+       
+    } while (ch != EOF);
  
     // Closing the file
     fclose(ptr);
